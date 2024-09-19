eda3feaaf2e45d1e620bdc763eab0684631c3e8e
Kevin Abaskaran <shiva94460@gmail.com>
2024-09-19 17:57:11 +0200

    ✨ Router and link done

---
+++ recipe-book/src/component/DashboardPage.jsx
@@ -0,0 +1,11 @@
+import List from "./List";
+
+const DashboardPage = () => {
+  return (
+    <div>
+      <h1>Dashboardpage</h1>
+      <List />
+    </div>
+  );
+};
+export default DashboardPage;
