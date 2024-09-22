2eee6865280f1f2fc054b0e4347d3bc9d9d34349
Kevin Abaskaran <shiva94460@gmail.com>
2024-09-22 11:21:18 +0200

    Revert "✨ Router and link done"
    This reverts commit eda3feaaf2e45d1e620bdc763eab0684631c3e8e.

--- recipe-book/src/component/Navbar.jsx
+++ recipe-book/src/component/Navbar.jsx
@@ -1,18 +1,10 @@
 import logo from "../assets/spatule-livre-recettes-creation-logo-vectoriel-livre_530862-377.jpg";
-import { Link } from "react-router-dom";
+
 function Navbar() {
   return (
     <nav>
       <img src={logo} alt="recipe-logo" className="logo" />
       <p>Recipe book</p>
-      <ul>
-        <li>
-          <Link to="/">Dashboard</Link>
-        </li>
-        <li>
-          <Link to="/about">About</Link>
-        </li>
-      </ul>
     </nav>
   );
 }
