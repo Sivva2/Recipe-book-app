f990d76787ca369f727e8d3ab3ac6eb9839093d8
Kevin Abaskaran <shiva94460@gmail.com>
2024-09-25 21:00:14 +0200

    🎨 Mini project Done

--- recipe-book/src/component/Footer.jsx
+++ recipe-book/src/component/Footer.jsx
@@ -1,7 +1,7 @@
 function Footer() {
   return (
-    <footer>
-      <a href="https://github.com/Sivva2/Recipe-book-app">github link</a>;
+    <footer className="footer">
+      <a href="https://github.com/Sivva2/Recipe-book-app"> github </a>
     </footer>
   );
 }
